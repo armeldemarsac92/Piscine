a8996e32188a4e1967d3fe9d21ea2c9c5b053683
armel <armeldemarsac@gmail.com>
2023-09-13 13:48:11 +0200

    start

--- day03/challenge.py
+++ day03/challenge.py
@@ -1,10 +1,10 @@
-n = int(input("combien de mots voulez vous rechercher"))
+n = int(input("combien de mots voulez vous rechercher ? "))
 phrase = "thE Cat’s tactic wAS tO surpRISE thE mIce iN tHE gArdeN"
 l =[]
 c = 0
 
 for i in range(n):
-    l.append(input("votre mot : "))
+    l.append(input(f"votre mot numéro {i} : "))
 
 for mot in l:
     c = c + phrase.lower().count(mot.lower()) + phrase.lower().count(mot[::-1].lower())
