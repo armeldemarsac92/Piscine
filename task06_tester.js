e5749b49f3d83d1e004e797ababcafcfcf5353ee
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-02 16:13:53 +0200

    .

--- task06_tester.js
+++
@@ -1,5 +0,0 @@
-import { objectsDeeplyEqual } from "./task06";
-const obj = {here: {is: "an"}, object: 2};
-console.log(objectsDeeplyEqual(obj, obj));
-console.log(objectsDeeplyEqual(obj, {here: 1, object: 2}));
-console.log(objectsDeeplyEqual(obj, {here: {is: "an"}, object: 2}));
