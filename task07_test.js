97ceea6ef3d782e04de7dd2e7a7d2b22084951da
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-02 16:14:13 +0200

    .

--- task07_test.js
+++
@@ -1,6 +0,0 @@
-import { arrayFiltering } from "./task07";
-const toFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const res = arrayFiltering(toFilter, function (value) {
-return value % 3 === 0;
-});
-console.log(res);
