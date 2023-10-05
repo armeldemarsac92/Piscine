e54d443c1832eb40182179fe42bf3823ccabc2dc
armeldemarsac92 <armeldemarsac@gmail.com>
2023-10-05 21:04:26 +0200

    push initial, exo 1

---
+++ index01.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang = " en ">
+<head>
+<meta charset = " utf -8 ">
+<title> Task 01 </title>
+</head>
+<body>
+<h1>hello world</h1>
+<?php
+require("./task01.php");
+$menu=display_menu ();
+echo $menu ;
+?>
+</body>
+</html>
